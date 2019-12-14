d4febd85631ee3c7d01b7d57e90100c7e27d6991
kleinfreund <philipprudloff@fastmail.com>
2019-12-14 20:04:39 +0100

    Add solution for day 7

---
+++ day-7/day-7-1.py
@@ -0,0 +1,225 @@
+#!/usr/bin/env python3
+
+from typing import List, Tuple
+import itertools
+
+
+OPCODE_ADD = '01'
+OPCODE_MULTIPLY = '02'
+OPCODE_INPUT = '03'
+OPCODE_OUTPUT = '04'
+OPCODE_JUMP_IF_TRUE = '05'
+OPCODE_JUMP_IF_FALSE = '06'
+OPCODE_LESS_THAN = '07'
+OPCODE_EQUALS = '08'
+OPCODE_HALT = '99'
+
+MODE_IMMEDIATE = '1'
+MODE_POSITION = '0'
+
+
+def read_intcode_program() -> List[int]:
+    """
+    Reads a text file called “orbit-data.txt”
+    and returns a list of strings representing direct orbit relationships.
+    """
+    import os
+    dir_path = os.path.dirname(os.path.realpath(
+        __file__)) + '/intcode-program.txt'
+    orbit_data = ''
+
+    with open(dir_path, 'r') as file:
+        orbit_data = file.read()
+        file.close()
+
+    return list(map(int, orbit_data.strip().split(',')))
+
+
+def read(intcode: List[int], index: int, param_mode: str) -> int:
+    """
+    Reads a value from the incode program according to its parameter mode.
+
+    Possible parameter modes are:
+
+    - MODE_POSITION: return the value of the intcode at `index`
+    - MODE_IMMEDIATE: return the value of `index` itself
+
+    >>> read([1002, 4, 3, 4, 33], 3, '0')
+    4
+    >>> read([1002, 4, 3, 4, 33], 4, '0')
+    33
+    >>> read([1002, 4, 3, 4, 33], 3, '1')
+    3
+    >>> read([3, 0, 4, 0, 99], 0, '0')
+    3
+    """
+    if param_mode == MODE_IMMEDIATE:
+        return index
+    else:
+        # Default to position mode (MODE_POSITION)
+        return intcode[index]
+
+
+def process_intcode(intcode: List[int], input_values: List[int] = None):
+    """
+    >>> process_intcode([3, 0, 4, 0, 99], [1])
+    1
+    >>> process_intcode([3, 0, 4, 0, 99], [222])
+    222
+    >>> process_intcode([3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8], [8])
+    1
+    >>> process_intcode([3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8], [7])
+    0
+    >>> process_intcode([3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8], [7])
+    1
+    >>> process_intcode([3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8], [8])
+    0
+    >>> process_intcode([3, 3, 1108, -1, 8, 3, 4, 3, 99], [8])
+    1
+    >>> process_intcode([3, 3, 1108, -1, 8, 3, 4, 3, 99], [1])
+    0
+    >>> process_intcode([3, 3, 1107, -1, 8, 3, 4, 3, 99], [7])
+    1
+    >>> process_intcode([3, 3, 1107, -1, 8, 3, 4, 3, 99], [8])
+    0
+    >>> process_intcode([3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9], [0])
+    0
+    >>> process_intcode([3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9], [1])
+    1
+    >>> process_intcode([3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1], [0])
+    0
+    >>> process_intcode([3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1], [1])
+    1
+    >>> process_intcode([3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31, 1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104, 999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99], [7])
+    999
+    >>> process_intcode([3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31, 1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104, 999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99], [8])
+    1000
+    >>> process_intcode([3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31, 1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104, 999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99], [9])
+    1001
+    """
+
+    output = None
+    input_counter = 0
+    pointer = 0
+    while (pointer < len(intcode)):
+        param_0 = format(intcode[pointer], '05d')
+        opcode = param_0[-2:]
+
+        if opcode == OPCODE_HALT:
+            break
+
+        if opcode == OPCODE_ADD:
+            param_1_mode = param_0[-3:-2]
+            input_1 = read(intcode, intcode[pointer + 1], param_1_mode)
+
+            param_2_mode = param_0[-4:-3]
+            input_2 = read(intcode, intcode[pointer + 2], param_2_mode)
+
+            intcode[intcode[pointer + 3]] = input_1 + input_2
+
+            pointer += 4
+        elif opcode == OPCODE_MULTIPLY:
+            param_1_mode = param_0[-3:-2]
+            input_1 = read(intcode, intcode[pointer + 1], param_1_mode)
+
+            param_2_mode = param_0[-4:-3]
+            input_2 = read(intcode, intcode[pointer + 2], param_2_mode)
+
+            intcode[intcode[pointer + 3]] = input_1 * input_2
+
+            pointer += 4
+        elif opcode == OPCODE_INPUT:
+            intcode[intcode[pointer + 1]] = input_values[input_counter]
+
+            pointer += 2
+            input_counter += 1
+        elif opcode == OPCODE_OUTPUT:
+            param_1_mode = param_0[-3:-2]
+            input_1 = read(intcode, intcode[pointer + 1], param_1_mode)
+            # print(input_1)
+            output = input_1
+
+            pointer += 2
+        elif opcode == OPCODE_JUMP_IF_TRUE:
+            param_1_mode = param_0[-3:-2]
+            input_1 = read(intcode, intcode[pointer + 1], param_1_mode)
+
+            if input_1 != 0:
+                param_2_mode = param_0[-4:-3]
+                input_2 = read(intcode, intcode[pointer + 2], param_2_mode)
+
+                pointer = input_2
+            else:
+                pointer += 3
+        elif opcode == OPCODE_JUMP_IF_FALSE:
+            param_1_mode = param_0[-3:-2]
+            input_1 = read(intcode, intcode[pointer + 1], param_1_mode)
+
+            if input_1 == 0:
+                param_2_mode = param_0[-4:-3]
+                input_2 = read(intcode, intcode[pointer + 2], param_2_mode)
+
+                pointer = input_2
+            else:
+                pointer += 3
+        elif opcode == OPCODE_LESS_THAN:
+            param_1_mode = param_0[-3:-2]
+            input_1 = read(intcode, intcode[pointer + 1], param_1_mode)
+            param_2_mode = param_0[-4:-3]
+            input_2 = read(intcode, intcode[pointer + 2], param_2_mode)
+
+            intcode[intcode[pointer + 3]] = 1 if input_1 < input_2 else 0
+
+            pointer += 4
+        elif opcode == OPCODE_EQUALS:
+            param_1_mode = param_0[-3:-2]
+            input_1 = read(intcode, intcode[pointer + 1], param_1_mode)
+            param_2_mode = param_0[-4:-3]
+            input_2 = read(intcode, intcode[pointer + 2], param_2_mode)
+
+            intcode[intcode[pointer + 3]] = 1 if input_1 == input_2 else 0
+
+            pointer += 4
+        else:
+            raise Exception('A very bad thing happened.')
+
+    return output
+
+
+def get_thruster_signal(intcode: List[int], phase_inputs: Tuple[int]) -> int:
+    """
+    >>> get_thruster_signal([3, 15, 3, 16, 1002, 16, 10, 16, 1, 16, 15, 15, 4, 15, 99, 0, 0], (4, 3, 2, 1, 0))
+    43210
+    >>> get_thruster_signal([3, 23, 3, 24, 1002, 24, 10, 24, 1002, 23, -1, 23, 101, 5, 23, 23, 1, 24, 23, 23, 4, 23, 99, 0, 0], (0, 1, 2, 3, 4))
+    54321
+    >>> get_thruster_signal([3, 31, 3, 32, 1002, 32, 10, 32, 1001, 31, -2, 31, 1007, 31, 0, 33, 1002, 33, 7, 33, 1, 33, 31, 31, 1, 32, 31, 31, 4, 31, 99, 0, 0, 0], (1, 0, 4, 3, 2))
+    65210
+    """
+    output = 0
+    for phase_input in phase_inputs:
+        output = process_intcode(intcode.copy(), [phase_input, output])
+    return output
+
+
+def find_max_thruster_signal(intcode: List[int]) -> int:
+    max_thruster_signal = 0
+
+    for phase_inputs in list(itertools.permutations(range(5), 5)):
+        thruster_signal = get_thruster_signal(intcode, phase_inputs)
+
+        if thruster_signal > max_thruster_signal:
+            max_thruster_signal = thruster_signal
+
+    return max_thruster_signal
+
+
+def main():
+    intcode = read_intcode_program()
+    max_thruster_signal = find_max_thruster_signal(intcode)
+    print(max_thruster_signal)
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    main()
---
+++ day-7/day-7-2.py
@@ -0,0 +1,242 @@
+#!/usr/bin/env python3
+
+from typing import Dict, List, Tuple
+import itertools
+import operator
+
+
+OPCODE_ADD = '01'
+OPCODE_MULTIPLY = '02'
+OPCODE_INPUT = '03'
+OPCODE_OUTPUT = '04'
+OPCODE_JUMP_IF_TRUE = '05'
+OPCODE_JUMP_IF_FALSE = '06'
+OPCODE_LESS_THAN = '07'
+OPCODE_EQUALS = '08'
+OPCODE_HALT = '99'
+
+MODE_IMMEDIATE = '1'
+MODE_POSITION = '0'
+
+
+def read_intcode_program() -> List[int]:
+    """
+    Reads a text file called “orbit-data.txt”
+    and returns a list of strings representing direct orbit relationships.
+    """
+    import os
+    dir_path = os.path.dirname(os.path.realpath(
+        __file__)) + '/intcode-program.txt'
+    orbit_data = ''
+
+    with open(dir_path, 'r') as file:
+        orbit_data = file.read()
+        file.close()
+
+    return list(map(int, orbit_data.strip().split(',')))
+
+
+def read(intcode: List[int], index: int, param_mode: str) -> int:
+    """
+    Reads a value from the incode program according to its parameter mode.
+
+    Possible parameter modes are:
+
+    - MODE_POSITION: return the value of the intcode at `index`
+    - MODE_IMMEDIATE: return the value of `index` itself
+
+    >>> read([1002, 4, 3, 4, 33], 3, '0')
+    4
+    >>> read([1002, 4, 3, 4, 33], 4, '0')
+    33
+    >>> read([1002, 4, 3, 4, 33], 3, '1')
+    3
+    >>> read([3, 0, 4, 0, 99], 0, '0')
+    3
+    """
+    if param_mode == MODE_IMMEDIATE:
+        return index
+    else:
+        # Default to position mode (MODE_POSITION)
+        return intcode[index]
+
+
+def relate(intcode, pointer, param_0, op):
+    param_1_mode = param_0[-3]
+    param_1_index = intcode[pointer + 1]
+    input_1 = read(intcode, param_1_index, param_1_mode)
+
+    param_2_mode = param_0[-4]
+    param_2_index = intcode[pointer + 2]
+    input_2 = read(intcode, param_2_index, param_2_mode)
+
+    return op(input_1, input_2)
+
+
+def jump_if(intcode, pointer, param_0, op):
+    param_1_mode = param_0[-3]
+    param_1_index = intcode[pointer + 1]
+    param_1_value = read(intcode, param_1_index, param_1_mode)
+
+    if op(param_1_value, 0):
+        param_2_mode = param_0[-4]
+        param_2_index = intcode[pointer + 2]
+        param_2_value = read(intcode, param_2_index, param_2_mode)
+
+        return param_2_value
+
+
+def process_intcode(program: Dict, input_values: List[int] = []) -> Tuple[bool, int]:
+    intcode = program['intcode']
+
+    while (program['pointer'] < len(intcode)):
+        pointer = program['pointer']
+
+        # Normalizes the format of intcode instructions:
+        # 1 → '00001'
+        # 1002 → '01002'
+        # This ensures that “unset” options default to '0'
+        param_0 = format(intcode[pointer], '05d')
+        opcode = param_0[-2:]
+
+        if opcode == OPCODE_HALT:
+            program['did_halt'] = True
+            return
+
+        if opcode == OPCODE_ADD:
+            result = relate(intcode, pointer, param_0, operator.add)
+            intcode[intcode[pointer + 3]] = result
+
+            program['pointer'] += 4
+        elif opcode == OPCODE_MULTIPLY:
+            result = relate(intcode, pointer, param_0, operator.mul)
+            intcode[intcode[pointer + 3]] = result
+
+            program['pointer'] += 4
+        elif opcode == OPCODE_INPUT:
+            param_1_index = intcode[pointer + 1]
+            intcode[param_1_index] = input_values[program['input_pointer']]
+
+            program['pointer'] += 2
+            program['input_pointer'] += 1
+        elif opcode == OPCODE_OUTPUT:
+            param_1_mode = param_0[-3]
+            param_1_index = intcode[pointer + 1]
+            param_1_value = read(intcode, param_1_index, param_1_mode)
+            program['output'].append(param_1_value)
+
+            program['pointer'] += 2
+            # Pause execution
+            return
+        elif opcode == OPCODE_JUMP_IF_TRUE:
+            param_2_value = jump_if(intcode, pointer, param_0, operator.ne)
+
+            if param_2_value:
+                program['pointer'] = param_2_value
+            else:
+                program['pointer'] += 3
+        elif opcode == OPCODE_JUMP_IF_FALSE:
+            param_2_value = jump_if(intcode, pointer, param_0, operator.eq)
+
+            if param_2_value:
+                program['pointer'] = param_2_value
+            else:
+                program['pointer'] += 3
+        elif opcode == OPCODE_LESS_THAN:
+            result = 1 if relate(intcode, pointer, param_0, operator.lt) else 0
+            param_3_index = intcode[pointer + 3]
+            intcode[param_3_index] = result
+
+            program['pointer'] += 4
+        elif opcode == OPCODE_EQUALS:
+            result = 1 if relate(intcode, pointer, param_0, operator.eq) else 0
+            param_3_index = intcode[pointer + 3]
+            intcode[param_3_index] = result
+
+            program['pointer'] += 4
+        else:
+            raise Exception('A very bad thing happened.')
+
+
+def get_thruster_signal(intcode: List[int], inputs: Tuple[int]) -> int:
+    """
+    >>> get_thruster_signal([3, 26, 1001, 26, -4, 26, 3, 27, 1002, 27, 2, 27, 1, 27, 26, 27, 4, 27, 1001, 28, -1, 28, 1005, 28, 6, 99, 0, 0, 5], (9, 8, 7, 6, 5))
+    139629729
+    >>> get_thruster_signal([3, 52, 1001, 52, -5, 52, 3, 53, 1, 52, 56, 54, 1007, 54, 5, 55, 1005, 55, 26, 1001, 54, -5, 54, 1105, 1, 12, 1, 53, 54, 53, 1008, 54, 0, 55, 1001, 55, 1, 55, 2, 53, 55, 53, 4, 53, 1001, 56, -1, 56, 1005, 56, 6, 99, 0, 0, 0, 0, 10], (9, 7, 8, 5, 6))
+    18216
+    """
+
+    program_a = {
+        'intcode': intcode.copy(),
+        'pointer': 0,
+        'input_pointer': 0,
+        'output': [],
+        'did_halt': False
+    }
+    program_b = {
+        'intcode': intcode.copy(),
+        'pointer': 0,
+        'input_pointer': 0,
+        'output': [],
+        'did_halt': False
+    }
+    program_c = {
+        'intcode': intcode.copy(),
+        'pointer': 0,
+        'input_pointer': 0,
+        'output': [],
+        'did_halt': False
+    }
+    program_d = {
+        'intcode': intcode.copy(),
+        'pointer': 0,
+        'input_pointer': 0,
+        'output': [],
+        'did_halt': False
+    }
+    program_e = {
+        'intcode': intcode.copy(),
+        'pointer': 0,
+        'input_pointer': 0,
+        'output': [],
+        'did_halt': False
+    }
+
+    program_e['output'] = [0]
+    program_did_halt = False
+
+    while not program_did_halt:
+        process_intcode(program_a, [inputs[0]] + program_e['output'])
+        process_intcode(program_b, [inputs[1]] + program_a['output'])
+        process_intcode(program_c, [inputs[2]] + program_b['output'])
+        process_intcode(program_d, [inputs[3]] + program_c['output'])
+        process_intcode(program_e, [inputs[4]] + program_d['output'])
+
+        program_did_halt = program_a['did_halt'] and program_b['did_halt'] and program_c[
+            'did_halt'] and program_d['did_halt'] and program_e['did_halt']
+
+    return program_e['output'][-1]
+
+
+def find_max_thruster_signal(intcode: List[int]) -> int:
+    max_thruster_signal = 0
+
+    for phase_inputs in list(itertools.permutations(range(5, 10), 5)):
+        thruster_signal = get_thruster_signal(intcode, phase_inputs)
+
+        if thruster_signal > max_thruster_signal:
+            max_thruster_signal = thruster_signal
+
+    return max_thruster_signal
+
+
+def main():
+    intcode = read_intcode_program()
+    max_thruster_signal = find_max_thruster_signal(intcode)
+    print(max_thruster_signal)
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    main()
